179ad9f631b725fb9c59a85323b4523721302cae
kondo-akihiro-git <2000akisan@gmail.com>
2025-07-09 14:12:03 +0900

    ユーザー設定処理更新

--- frontend/src/network/useGetUser.js
+++ frontend/src/network/useGetUser.js
@@ -1,7 +1,7 @@
 // src/network/useGetUser.js
 export async function useGetUser(user_id) {
   try {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/user/${user_id}`)
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/user/${user_id}/info`)
     const data = await response.json()  
     return data
   } catch (error) {
